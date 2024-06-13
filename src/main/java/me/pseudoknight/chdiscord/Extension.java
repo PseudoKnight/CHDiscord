1bd904b41986d0b66f96d54ebf7a720cd62f69e9
PseudoKnight <michael.jt.smith@gmail.com>
2024-06-13 06:36:19 -0700

    Bump to 2.13.1-SNAPSHOT

--- src/main/java/me/pseudoknight/chdiscord/Extension.java
+++ src/main/java/me/pseudoknight/chdiscord/Extension.java
@@ -14,7 +14,7 @@ import java.util.Map;
 public class Extension extends AbstractExtension {
 
 	public Version getVersion() {
-		return new SimpleVersion(2,13,0);
+		return new SimpleVersion(2,13,1, "SNAPSHOT");
 	}
 
 	@Override
