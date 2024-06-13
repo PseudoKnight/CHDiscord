6f8b00e345d7b33226d952980ca56a36ad3d5b9b
PseudoKnight <michael.jt.smith@gmail.com>
2024-06-13 11:03:00 -0700

    Release 2.13.2

--- src/main/java/me/pseudoknight/chdiscord/Extension.java
+++ src/main/java/me/pseudoknight/chdiscord/Extension.java
@@ -14,7 +14,7 @@ import java.util.Map;
 public class Extension extends AbstractExtension {
 
 	public Version getVersion() {
-		return new SimpleVersion(2,13,1);
+		return new SimpleVersion(2,13,2);
 	}
 
 	@Override
